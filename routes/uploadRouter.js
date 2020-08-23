d717330535c34a804684cc79a6e67a397597439e
Eddges <shivamshekhar.i7@gmail.com>
2020-08-24 02:06:52 +0530

    upload api added, process env PUBLIC_URL accessed

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -3,21 +3,43 @@ const bodyParser = require('body-parser')
 const User = require('../models/userModel')
 const authenticate = require('../config/authenticate')
 const multer = require('multer')
+const path = require('path')
 
 const uploadRouter = express.Router()
 uploadRouter.use(bodyParser.json())
 
 const storage = multer.diskStorage({
     destination : (req, file, callback) => {
-        callaback(null, 'public/images')
+        callback(null, 'public/images')
+    },
+    filename : (req, file, callback) => {
+        callback(null, `${req.user.username}${path.extname(file.originalname)}`)
     }
 })
 
+const imageFileFilter = (req, file, callback) => {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+        return callback(new Error("Server can only handle image files. Please proceed accordingly"))
+    }
+    callback(null, true)
+}
+
+const upload = multer({storage : storage, fileFilter : imageFileFilter})
+
 uploadRouter.route('/')
-.get(authenticate.verifyUser, (req, res, next) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.json({user : req.user})
+.post(authenticate.verifyUser, upload.single('imageFile'), (req, res) => {
+    User.findById(req.user._id)
+    .then(user => {
+        console.log('file destination : ', req.file.destination)
+        user.imageLocation = req.file.path
+        console.log('imageLocation : ', user.imageLocation)
+        user.save()
+        .then(doc => {
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json')
+            res.json({user : user, file : req.file})
+        })
+    })
 })
 
 module.exports = uploadRouter
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+// import image from '../public/images/shekharlol.jpg'
 
 class App extends React.Component{
   render(){
     return(
       <div>
-        App
+        <img src={process.env.PUBLIC_URL + '/images/shekharlol.jpg'} alt="Wont show" />
       </div>
     )
   }
